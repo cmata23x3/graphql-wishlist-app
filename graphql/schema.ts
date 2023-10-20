2c268980a1b8e33803566dee348b3a346b22da4a
Christian Mata <christian.m.mata@gmail.com>
2023-10-20 12:57:40 -0500

    Add Item object type to GraphQL schema

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,6 +1,21 @@
-import { graphql } from "graphql";
-import { makeSchema, queryType, mutationType } from "nexus";
+import { asNexusMethod, makeSchema, queryType, mutationType, objectType } from "nexus";
 import * as path from 'path';
+import { DateTimeResolver } from "graphql-scalars";
+
+const DateTime = asNexusMethod(DateTimeResolver, 'DateTime')
+
+const Item = objectType({
+    name: 'Item',
+    definition(t) {
+        t.nonNull.id('id')
+        t.nonNull.string('title'),
+        t.string('description'),
+        t.string('url'),
+        t.string('imageUrl'),
+        t.field('createdAt', { type: 'DateTime'}),
+        t.field('updatedAt', { type: 'DateTime' })
+    }
+})
 
 const Query = queryType({
     definition(t) {
@@ -15,7 +30,7 @@ const Mutation = mutationType({
 })
 
 export const schema = makeSchema({
-    types: [Query, Mutation],
+    types: [Query, Mutation, DateTime, Item],
     outputs: {
         schema: path.join(process.cwd(), 'graphql/schema.graphql'),
         typegen: path.join(process.cwd(), 'graphql/generated/nexus.d.ts'),
