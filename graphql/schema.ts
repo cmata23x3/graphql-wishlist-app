bcfa6ff87ce28319471a565b9c6faeb49e0e88dc
Christian Mata <christian.m.mata@gmail.com>
2023-10-20 13:59:31 -0500

    Add Query type definitions

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,8 +1,13 @@
-import { asNexusMethod, makeSchema, queryType, mutationType, objectType } from "nexus";
+import { asNexusMethod, makeSchema, queryType, mutationType, objectType, enumType, arg, nonNull, stringArg } from "nexus";
 import * as path from 'path';
 import { DateTimeResolver } from "graphql-scalars";
 
 const DateTime = asNexusMethod(DateTimeResolver, 'DateTime')
+
+const SortOrder = enumType({
+    name: "SortOrder",
+    members: ["asc", "desc"]
+})
 
 const Item = objectType({
     name: 'Item',
@@ -19,7 +24,31 @@ const Item = objectType({
 
 const Query = queryType({
     definition(t) {
+        t.list.field('getItems', {
+            type: 'Item',
+            args: {
+                sortBy: arg({type: 'SortOrder'}),
+            },
+            resolve: async (_, args, ctx) => {
+                return ctx.db.item.findMany({
+                    orderBy: { createdAt: args.sortBy || undefined }
+                })
+            }
+        })
 
+        t.field('getOneItem', {
+            type: 'Item',
+            args: {
+                id: nonNull(stringArg())
+            },
+            resolve: async (_, args, ctx) => {
+                try {
+                    return ctx.db.item.findUnique({ where: { id: args.id }})
+                } catch (error) {
+                    throw new Error(`${error}`)
+                }
+            }
+        })
     }
 })
 
@@ -30,7 +59,7 @@ const Mutation = mutationType({
 })
 
 export const schema = makeSchema({
-    types: [Query, Mutation, DateTime, Item],
+    types: [Query, Mutation, DateTime, Item, SortOrder],
     outputs: {
         schema: path.join(process.cwd(), 'graphql/schema.graphql'),
         typegen: path.join(process.cwd(), 'graphql/generated/nexus.d.ts'),
